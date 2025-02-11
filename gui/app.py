611032c547ba35e881a01b8d9600f37f073494ea
Henrique Trevisan <trehen30@gmail.com>
2025-02-10 21:38:01 -0300

    Refactor GUI by creating MyYouTubeDownloaderApp class for better structure and maintainability

--- gui/app.py
+++ gui/app.py
@@ -1,8 +1,7 @@
 import customtkinter as ctk
-from gui.components import MyScrollableRadioButtonFrame
+from gui.components import MyScrollableRadioButtonFrame, MyYouTubeDownloaderApp
 from backend.downloader import Downloader
 from backend.worker import Worker
-from utils.helpers import change_save_path
 import queue
 
 class App(ctk.CTk):
@@ -18,64 +17,12 @@ class App(ctk.CTk):
 
         self.selected_format = ctk.StringVar()
         self.selected_format.set(True)
+        self.selected_format.trace_add("write", self.update_download_button)
         self.checkbox_var = ctk.BooleanVar()
         self.checkbox_var.set(True)
 
-        self.selected_format = ctk.StringVar()
-        self.selected_format.trace_add("write", self.update_download_button)
-
         self.message_label = None  # Placeholder for messages
-        self.init_gui()
-
-    def init_gui(self) -> None:
-        # Configure grid
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure([1, 2], weight=0)
-
-        # Save Path Frame
-        self.Save_Frame = ctk.CTkFrame(self, corner_radius=0)
-        self.Save_Frame.grid(row=1, column=0, sticky="nwe")
-        self.Save_Frame.grid_columnconfigure(0, weight=0)
-        self.Save_Frame.grid_columnconfigure(1, weight=2)
-        self.Save_Frame.grid_columnconfigure(2, weight=0)
-
-        self.Save_Label = ctk.CTkLabel(self.Save_Frame, text="Save to: ")
-        self.Save_Label.grid(row=0, column=0, padx=[20, 5], pady=20, sticky="w")
-
-        self.Save_Entry = ctk.CTkEntry(self.Save_Frame, placeholder_text="File path")
-        self.Save_Entry.grid(row=0, column=1, padx=[0, 5], pady=20, sticky="ew")
-
-        change_button_text = "Change"
-        self.Save_Button = ctk.CTkButton(self.Save_Frame, text=change_button_text, 
-                                         command=lambda: change_save_path(self.Save_Entry))
-        self.Save_Button.grid(row=0, column=2, padx=[0, 20], pady=20, sticky="e")
-
-        # URL Frame
-        self.URL_Frame = ctk.CTkFrame(self, corner_radius=0)
-        self.URL_Frame.grid(row=2, column=0, sticky="nwe")
-        self.URL_Frame.grid_columnconfigure(0, weight=2)
-        self.URL_Frame.grid_columnconfigure([1, 2], weight=0)
-
-        self.URL_Label = ctk.CTkLabel(self, text="YouTube Video Downloader", font=("Consolas", 24))
-        self.URL_Label.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
-
-        self.URL_Entry = ctk.CTkEntry(self.URL_Frame, placeholder_text="Video URL")
-        self.URL_Entry.grid(row=1, column=0, padx=[20, 5], pady=20, sticky="ew")
-
-        self.Paste_URL_Button = ctk.CTkButton(
-            self.URL_Frame, text="Paste URL", 
-            command=lambda: self.URL_Entry.insert(0, self.clipboard_get())
-        )
-        self.Paste_URL_Button.grid(row=1, column=1, padx=[0, 5], pady=20, sticky="e")
-
-        self.Clear_URL_Button = ctk.CTkButton(
-            self.URL_Frame, text="Clear URL", 
-            command=lambda: self.URL_Entry.delete(0, "end")
-        )
-        self.Clear_URL_Button.grid(row=1, column=2, padx=[0, 20], pady=20, sticky="w")
-
-        self.Search_Button = ctk.CTkButton(self.URL_Frame, text="Search Video", command=lambda: self.task_queue.put((self.search_video, ())))
-        self.Search_Button.grid(row=2, column=0, padx=20, pady=[0, 20], columnspan=3)
+        MyYouTubeDownloaderApp(self)
 
     def search_video(self):
         self.show_message("Searching for video...")
--- gui/components.py
+++ gui/components.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from utils.helpers import change_save_path
 
 class MyScrollableRadioButtonFrame(ctk.CTkScrollableFrame):
     def __init__(self, master, title, values, variable):
@@ -26,3 +27,58 @@ class MyScrollableRadioButtonFrame(ctk.CTkScrollableFrame):
             radiobutton = ctk.CTkRadioButton(self, text=value, variable=self.variable, value=value)
             radiobutton.grid(row=i + 1, column=0, columnspan=4, padx=10, pady=(10, 0), sticky="ew")
             self.radiobuttons.append(radiobutton)
+
+class MyYouTubeDownloaderApp(ctk.CTk):
+    def __init__(self, master):
+        self.master = master
+        self.init_gui()
+
+    def init_gui(self) -> None:
+        # Configure grid
+        self.master.grid_columnconfigure(0, weight=1)
+        self.master.grid_rowconfigure([1, 2], weight=0)
+
+        # Save Path Frame
+        self.master.Save_Frame = ctk.CTkFrame(self.master, corner_radius=0)
+        self.master.Save_Frame.grid(row=1, column=0, sticky="nwe")
+        self.master.Save_Frame.grid_columnconfigure(0, weight=0)
+        self.master.Save_Frame.grid_columnconfigure(1, weight=2)
+        self.master.Save_Frame.grid_columnconfigure(2, weight=0)
+
+        self.master.Save_Label = ctk.CTkLabel(self.master.Save_Frame, text="Save to: ")
+        self.master.Save_Label.grid(row=0, column=0, padx=[20, 5], pady=20, sticky="w")
+
+        self.master.Save_Entry = ctk.CTkEntry(self.master.Save_Frame, placeholder_text="File path")
+        self.master.Save_Entry.grid(row=0, column=1, padx=[0, 5], pady=20, sticky="ew")
+
+        change_button_text = "Change"
+        self.master.Save_Button = ctk.CTkButton(self.master.Save_Frame, text=change_button_text, 
+                                         command=lambda: change_save_path(self.master.Save_Entry))
+        self.master.Save_Button.grid(row=0, column=2, padx=[0, 20], pady=20, sticky="e")
+
+        # URL Frame
+        self.master.URL_Frame = ctk.CTkFrame(self.master, corner_radius=0)
+        self.master.URL_Frame.grid(row=2, column=0, sticky="nwe")
+        self.master.URL_Frame.grid_columnconfigure(0, weight=2)
+        self.master.URL_Frame.grid_columnconfigure([1, 2], weight=0)
+
+        self.master.URL_Label = ctk.CTkLabel(self.master, text="YouTube Video Downloader", font=("Consolas", 24))
+        self.master.URL_Label.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+
+        self.master.URL_Entry = ctk.CTkEntry(self.master.URL_Frame, placeholder_text="Video URL")
+        self.master.URL_Entry.grid(row=1, column=0, padx=[20, 5], pady=20, sticky="ew")
+
+        self.master.Paste_URL_Button = ctk.CTkButton(
+            self.master.URL_Frame, text="Paste URL", 
+            command=lambda: self.master.URL_Entry.insert(0, self.master.clipboard_get())
+        )
+        self.master.Paste_URL_Button.grid(row=1, column=1, padx=[0, 5], pady=20, sticky="e")
+
+        self.master.Clear_URL_Button = ctk.CTkButton(
+            self.master.URL_Frame, text="Clear URL", 
+            command=lambda: self.master.URL_Entry.delete(0, "end")
+        )
+        self.master.Clear_URL_Button.grid(row=1, column=2, padx=[0, 20], pady=20, sticky="w")
+
+        self.master.Search_Button = ctk.CTkButton(self.master.URL_Frame, text="Search Video", command=lambda: self.master.task_queue.put((self.master.search_video, ())))
+        self.master.Search_Button.grid(row=2, column=0, padx=20, pady=[0, 20], columnspan=3)
